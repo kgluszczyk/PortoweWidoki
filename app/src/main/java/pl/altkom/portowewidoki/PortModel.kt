3974d83b48b8ac39cf2c5276a2092b4b7f393dc2
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-08-04 14:54:06 +0200

    Add ports storage

--- app/src/main/java/pl/altkom/portowewidoki/PortModel.kt
+++ app/src/main/java/pl/altkom/portowewidoki/PortModel.kt
@@ -6,5 +6,14 @@ data class PortModel(val name: String, val location: String, @DrawableRes val im
 
 object PortStorage {
 
-    val ports = listOf(PortModel(name = "Port1", location = "PL", image = R.drawable.port))
+    val ports = listOf(
+        PortModel(name = "Port1", location = "PL", image = R.drawable.port),
+        PortModel(name = "Port2", location = "PL", image = R.drawable.port_2),
+        PortModel(name = "Port3", location = "PL", image = R.drawable.port_3),
+        PortModel(name = "Port4", location = "PL", image = R.drawable.port_4),
+        PortModel(name = "Port5", location = "PL", image = R.drawable.port_5),
+        PortModel(name = "Port6", location = "PL", image = R.drawable.port_6),
+        PortModel(name = "Port7", location = "PL", image = R.drawable.port_7),
+        PortModel(name = "Port8", location = "DE", image = R.drawable.port_8),
+    )
 }

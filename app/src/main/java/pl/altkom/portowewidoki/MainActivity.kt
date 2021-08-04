c5a0d0807caf5d454ba18ba98ca010ba6ecff748
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-08-04 11:14:49 +0200

    Activity lifecycle

--- app/src/main/java/pl/altkom/portowewidoki/MainActivity.kt
+++ app/src/main/java/pl/altkom/portowewidoki/MainActivity.kt
@@ -2,11 +2,60 @@ package pl.altkom.portowewidoki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "LIFECYCLE"
+    val ResumeCountKey = "ResumeCountKey"
+    var onResumeCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(tag, "ON_CREATE")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(ResumeCountKey, onResumeCount)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(BuildConfig.DEBUG) {
+            Log.d(tag, "ON_START")
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "ON_RESTART")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onResumeCount++
+        Log.d(tag, "ON_RESUME_$onResumeCount")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "ON_PAUSE")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "ON_STOP")
+    }
+
+    override fun onDestroy() {
+        Log.d(tag, "ON_DESTROY")
+        super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        Log.d(tag, "ON_BACK_PRESSED")
+        super.onBackPressed()
     }
 }
